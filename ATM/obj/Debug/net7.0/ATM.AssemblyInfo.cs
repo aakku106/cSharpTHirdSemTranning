889b51c4b11bb5b150fac5321269f57a41be92ce
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-01-17 00:08:27 +0545

    Enhance user creation process with validation and error handling in Index page

--- ATM/obj/Debug/net7.0/ATM.AssemblyInfo.cs
+++ ATM/obj/Debug/net7.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee22da38393c72ee986148ec96b9299dfc04eb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4630c3dd86a6aa2b3c1d1ea4228c3079b7f2d339")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
