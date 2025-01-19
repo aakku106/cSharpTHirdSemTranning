f38eebdfed47b1ec2bc162ab5dbe87cb0050416c
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-01-19 19:59:57 +0545

    Enhance user details display with password protection and improve deposit/withdraw functionality in Index page

--- ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc73fb5b3d902d2b5d233167bfac56f209462e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374024a6f5d68153e079ff6a5dcce02d2c79e9a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
