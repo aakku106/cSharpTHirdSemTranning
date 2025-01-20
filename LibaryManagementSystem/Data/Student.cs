bd34a10bbe0c3747a7d50684bbdb63690e1ee5ed
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-01-20 21:06:31 +0545

    Update student data with new entries and modify existing student details

--- LibaryManagementSystem/Data/Student.cs
+++ LibaryManagementSystem/Data/Student.cs
@@ -7,11 +7,18 @@ namespace LibaryManagementSystem.Data
 
         public List<Students> student = new List<Students>(){
             new Students(){
-                Id="106",
+                Id="AF10",
                 Name="ccn",
-                Class=5,
+                Class=null,
                 Address="ccn-106, Konoha",
                 Password="ccn"
+            },
+             new Students(){
+                Id="106",
+                Name="AAKKU",
+                Class=10,
+                Address="your-Heart-106",
+                Password="!)^"
             }
         };
 
