8634fb3148fd0276def6d1de356ea76f56582e87
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-15 19:50:23 +0545

    Add new binaries and configuration files for macOs project targeting .NET 9.0

--- ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7cf434b668d55c6bc280643c542b79f4cc2188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19207791baeae180cb816fc4a9a2d029f536dd37")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
