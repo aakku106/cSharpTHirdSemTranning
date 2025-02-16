e408d4373bcbfd4ba29fd5f1a409b8a00aa67bdd
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-02-16 21:20:54 +0545

    Update binaries and assembly information for macOs project; remove unused JavaScript asset and adjust static web assets configuration

--- Practise/macOs/obj/Debug/net9.0/macOs.AssemblyInfo.cs
+++ Practise/macOs/obj/Debug/net9.0/macOs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("macOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ab76c62666eec837106f0af2f52100aa9764bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e8f59f85b8bf1ed52573c1b52f30a867346f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("macOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("macOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
